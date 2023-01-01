d6b3d67c0c685a8ebcb6f1446a831cca5708b752
JasonSKK <ko00@windowslive.com>
2023-01-01 13:46:00 +0200

    upd readme

--- old_script_config/gmi-sync.sh
+++
@@ -1,7 +0,0 @@
-#220414 Jason S.K.
-#this script uses lieer to fetch new email
-#cd to config directory
-#gmi pushes and pulls new emails
-#popd to previous dir
-#notmuch new: Find and import new messages to the notmuch database.
-pushd $1 ; gmi sync ; popd ; notmuch new
